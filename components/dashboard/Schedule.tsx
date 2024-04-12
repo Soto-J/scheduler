c62754089deb6796da1d20c48eae13eaf42563ce
John Soto <soto.ja415@gmail.com>
2024-04-12 17:31:21 -0400

    update

--- components/dashboard/Schedule.tsx
+++ components/dashboard/Schedule.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+} from "../ui/select";
+
 type ScheduleProps = {
   date: Date | undefined;
 };
@@ -5,8 +16,24 @@ type ScheduleProps = {
 export const Schedule = ({ date }: ScheduleProps) => {
   return (
     <div>
-      <h2>Schedule</h2>
-      <div>Date: {JSON.stringify(date)}</div>
+      <h2>Date: {JSON.stringify(date)}</h2>
+
+      <div className="flex gap-x-3 pt-4 items-center">
+        <p>Available timeslots:</p>
+        <Select>
+          <SelectTrigger>Choose a time slot...</SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Time Slows</SelectLabel>
+              <SelectItem value="9">9</SelectItem>
+              <SelectItem value="11">11</SelectItem>
+              <SelectItem value="1">1</SelectItem>
+              <SelectItem value="3">3</SelectItem>
+              <SelectItem value="5">5</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   );
 };
