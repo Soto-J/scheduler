84367c88c3db4d5d03032fee727a156015544680
John Soto <soto.ja415@gmail.com>
2024-04-12 16:39:52 -0400

    Fixed issue with running app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-
+import { Calendar } from "@/components/ui/calendar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24"></div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Calendar />
+    </div>
   );
 }
